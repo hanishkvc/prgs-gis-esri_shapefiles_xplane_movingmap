063e7eb387e3fdaab98de58ff4fa929bfd8a08e9
hanishkvc <hanishkvc@gmail.com>
2014-12-02 08:41:00 +0530

    ESRI GIS Map Shapefile tools
    Added a basic version of python based ESRI SHP file parser and plotter
    
    * It uses python struct to read thro binary records
      * It uses standard mode rather than native mode so that it can
        operate on packed records/structures
    
      It uses turtle graphics to plot
    
    * It can read and interpret the shp file header
      * It checks for SHAPEFILE_CODE (9994)
      * It doesn't check version for now
      * A SHAPETYPES dictionary is defined to map between the shapetype code
        and its human readable string representation. Useful Mainly for print
      * It calculates the file size from header
      * It extracts the bounding box of the given shapefile from header
      * Currently I am manually assigning each header item 1 at a time by
        using a index operation. Instead I could have assigned them in
        one shot. i.e (v1,v2,v3,...) = hdrP2 (or struct.unpack)
      * NOTE: Even thou ESRI Shapefile spec says that a given shapefile for now
        will only contain records belonging to one type of shapetypes,
        as identified in the header, The logic implemented doesn't restrict
        itself to this. The logic is capable of working with more than
        one shapetype in a given shapefile (and it is aided in this by the
        availability of shapetype info in each record)
    
    * It can read the records in the shapefile 1 at a time and pass it to
      suitable handler for the given shapetype
      * It reads the fixed/common record header part of each record
      * Use the record length info retrieved to read the full record into
        a bytes object. (The File opened in binary mode to read bytes rather
        than strings)
      * It identifies the shapetype stored in the record and calls the
        corresponding handler
        * As of now only polygon shapetype is handled. Others if any will
          be dropped/ignored (so that next record can be processed as
          required)
    
    * The Polygon shapetype record handler reads the header for the given
      shapetype as well as handles the possibility of more than one polygon
      being specified in the record.
      * As noted in the spec file, the multiple polygons if specified will
        have their vertices stored serially 1 after the other. And the Parts
        Array will store the index into the Points array, and this inturn
        identifies the 1st vertice of each polygon shape within the record.
        * A Helper function helps extract the begining and ending index into
          points array for each polygon (called part)
      * It also plots each of the polygon.
        * It scales each point's x and y by 3 for now because the lat, lon
          corresponds to 90_to_-90 and -180_to_180 i.e Y axis 180 and X axis
          360 so it will plot over a area of 540 in Y axis and 1080 in Y axis
    
    * It checks that end of file has been reached using a exception and then
      verifies that the current file position inturn matches the calculated
      file size.

---
+++ hkvc-map-esri-shapefile-shp.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+#
+# ESRI Map/GIS Shapefile SHP parser and plotter
+# HanishKVC, 2014
+# GPL
+#
+
+import struct
+import sys
+import turtle
+
+SHAPEFILE_CODE = 9994
+SHAPETYPES = { 0: "NullShape", 1: "Point", 3: "PolyLine", 5: "Polygon", 8: "MultiPoint", 11: "PointZ", 13: "PolyLineZ", 15: "PolygonZ", 
+		18: "MultiPointZ", 21: "PointM", 23: "PolyLineM", 25: "PolygonM", 28: "MultiPointM", 31: "MultiPatch" }
+
+gFileLength = 0
+
+gTr = None
+
+def shp_read_fileheader(f):
+	global gFileLength
+
+	#7 BigEndian Integers
+	#2 LittleEndian Integers
+	#8 LittleEndian Double
+	hdrDataBE=f.read(28)
+	hdrP1=struct.unpack(">7i",hdrDataBE)
+	hdrDataLE=f.read(72)
+	hdrP2=struct.unpack("<2i8d",hdrDataLE)
+	print(hdrP1,hdrP2)
+
+	tFileCode=hdrP1[0]
+	if (tFileCode != SHAPEFILE_CODE):
+		print("ERROR: Not a ShapeFile, Quiting...")
+		exit()
+	else:
+		print("INFO: This is a ShapeFile, Continuing...")
+
+	tFileLength=hdrP1[6]*2	# Because size is mentioned in 16bit word  units
+	gFileLength = tFileLength
+	tVersion=hdrP2[0]
+	tShapeType=hdrP2[1]
+	tShapeTypeStr=SHAPETYPES[tShapeType]
+	gXMin = hdrP2[2]
+	gYMin = hdrP2[3]
+	gXMax = hdrP2[4]
+	gYMax = hdrP2[5]
+	gZMin = hdrP2[6]
+	gZMax = hdrP2[7]
+	gMMin = hdrP2[8]
+	gMMax = hdrP2[9]
+
+	print("FileCode[{}]\nFileLength[{}]\nVersion[{}]".format(tFileCode, tFileLength, tVersion))
+	print("ShapeType used in this ShapeFile is [{}]".format(tShapeTypeStr))
+	print("BoundingBox:\n\tXMin[{}],YMin[{}],XMax[{}],YMax[{}]".format(gXMin,gYMin,gXMax,gYMax))
+	print("\tZMin[{}],ZMax[{}],MMin[{}],MMax[{}]".format(gZMin,gZMax,gMMin,gMMax))
+
+
+def shp_read_records(f):
+	while True:
+		rHdrData = f.read(8)
+		(recNum, recContentLength) = struct.unpack(">2i",rHdrData)
+		recContentLength = recContentLength*2 # Convert for 16bit word size to 8bit byte size
+		print("Processing Record[{}] of size[{}]".format(recNum,recContentLength))
+		rContentData = f.read(recContentLength)
+		rShapeType = rContentData[0:4]
+		rShapeType = int.from_bytes(rShapeType,'little')
+		print(SHAPETYPES[rShapeType])
+		if (rShapeType == 5):
+			shp_read_polygon(rContentData)
+
+
+def shp_read_polygon(data):
+	# Each polygon shape record contains multiple polygons whose vertices are stored serially one after the other
+	hdrPolygonData = data[0:44]
+	hdrPolygon = struct.unpack("<i4d2i",hdrPolygonData)
+	(pShapeType, pBBXMin, pBBYMin, pBBXMax, pBBYMax, pNumParts, pNumPoints) = hdrPolygon
+	print(hdrPolygon)
+
+	polyStartPointIndexesArrayStart = 44
+	polyStartPointIndexesArrayEnd = polyStartPointIndexesArrayStart + 4*pNumParts
+	polyStartPointIndexesArrayData = data[polyStartPointIndexesArrayStart:polyStartPointIndexesArrayEnd]
+	# This array contains indexes to the polyPointsArray, which specifies the 1st point of the given/current polygon
+	# The 1st point of the next polygon automatically means that the corresponding previous point is the end of the current polygon
+	polyStartPointIndexesArray = struct.unpack("<{}i".format(pNumParts), polyStartPointIndexesArrayData) 
+
+	polyPointsArrayStart = polyStartPointIndexesArrayEnd
+	polyPointsArrayEnd = polyPointsArrayStart + (8*2)*pNumPoints
+	if (polyPointsArrayEnd != len(data)):
+		print("DEBUG: Something wrong with Polygon Shape record parsing, Quiting...")
+		exit()
+	polyPointsArrayData = data[polyPointsArrayStart:polyPointsArrayEnd]
+
+	#NOTE: FIXME: This is a simple temp logic which ignores about multiple Polygons within a single Polygon shape record content
+	cPoly = 0
+	(cPointStart, cPointEnd) = shp_poly_startend(polyStartPointIndexesArray, cPoly, pNumParts, pNumPoints)
+	for i in range(0,pNumPoints):
+		cPoint = struct.unpack_from("<2d", polyPointsArrayData, i*16)
+		print(cPoint)
+		#gTr.goto(cPoint)
+		gTr.goto(cPoint[0]*3,cPoint[1]*3)
+		if (i == cPointStart):
+			gTr.pendown()
+		if (i == cPointEnd):
+			gTr.penup()
+			cPoly = cPoly+1
+			(cPointStart, cPointEnd) = shp_poly_startend(polyStartPointIndexesArray, cPoly, pNumParts, pNumPoints)
+
+
+
+def shp_poly_startend(polyStartPointIndexesArray, curPoly, numPolys, numPoints):
+	if (curPoly == numPolys):
+		cPointStart = -1
+		cPointEnd = -1
+	else:
+		cPointStart = polyStartPointIndexesArray[curPoly]
+		if ((curPoly+1) == numPolys):
+			cPointEnd = numPoints-1
+		else:
+			cPointEnd = polyStartPointIndexesArray[curPoly+1]-1
+	return (cPointStart, cPointEnd)
+
+
+def main():
+	global gTr
+	gTr = turtle
+	gTr.penup()
+	f=open(sys.argv[1],"rb")
+	shp_read_fileheader(f)
+	try:
+		shp_read_records(f)
+	except:
+		print(sys.exc_info())
+		if (gFileLength == f.tell()):
+			print("INFO: Seems like End of File![{}]".format(f.tell()))
+		else:
+			print("WARN: Unexpected FileLocation?[{}]".format(f.tell()))
+
+	f.close()
+	
+
+main()
+input("Hope the shapefile was plotted well...")
