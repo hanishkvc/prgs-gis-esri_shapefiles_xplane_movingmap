4ac1d1c52dbfb00e367c604614d6c17e8739031b
hanishkvc <hanishkvc@gmail.com>
2014-12-06 01:41:00 +0530

    ESRI Shapefile DBF Parser Update (A Module with Class now)
    * Finished the conversion of this program into a Module with a class which
      provides DBF parsing and usage support.
    
    * Moved the Global CONSTANTS related to Fields Dictionary value list offsets
      to be Class Variables (NOTE: NOT Class Instance Variables)
    
    * Moved all the functions associated with the basic DBF file parsing and usage
      to be class functions
    
      In turn they now get called using the self function argument for getting
      reference to themselves
    
    * Added a new class function which checks if a given field (i.e fieldname)
      exists in the dbase file records or not.
    
    * Renamed the dbf_read function has dbf_load
    
    * Updated the dbf_close function so that it calls the __init__ function
    
      So rather the same class instance can be used to operate on more than
      dbase file one after the another in a serial manner by using multiple pairs
      of dbf_load and dbf_close calls. However you can always use multiple class
      instances to work with multiple dbase files parallely.
    
    * Modified the previous dbf_test function to be the logic which runs if this
      module is directly called has a program (i.e not imported into another
      program). In turn the logic updated to use the new dbf parser class defined
      by this module.

--- hkvc_map_esri_shapefile_dbf.py
+++ hkvc_map_esri_shapefile_dbf.py
@@ -13,81 +13,92 @@ import turtle
 
 class DbfParser:
 
-	def __init__():
+	FIELD_OFFSET = 0
+	FIELD_TYPE = 1
+	FIELD_LEN = 2
+
+	def __init__(self):
 		self.dbNumRecs = 0
 		self.dbRecStartOffset = 0
 		self.dbRecLen = 0
-		FIELD_OFFSET = 0
-		FIELD_TYPE = 1
-		FIELD_LEN = 2
 		self.dbRecFields = {}
 
-def dbf_read_fileheader(f):
+	def dbf_read_fileheader(self):
 
-	hdrData = self.dbFile.read(32)
-	hdr = struct.unpack("<4Bi3h2B3i2Bh",hdrData)
-	#print(hdr)
-	(dbVer,uYY,uMM,uDD,numRecs,lenHdr,lenRec,hRsvd1,bDirty,bEncrypted,iFreeRecThread,iRsvd2,iRsvd3,bMdx,langDrvr,hRsvd4) = hdr
-	if (dbVer != 3):
-		print("ERROR: Dont understand the dBase dbf file format version...")
-		exit()
-	else:
-		print("INFO: dBase III w/o memo file, File without DBT")
-	print("INFO:FileHeader\n\tnumRecs[{}]\n\tlenHdr[{}]\n\tlenRec[{}]".format(numRecs,lenHdr,lenRec))
-	self.dbNumRecs = numRecs
-	iFOffset = 1 # This is to take care of the 1 byte record deleted or not flag at beginning of each record
-	while True:
-		fdHdrData = self.dbFile.read(32)
-		if (fdHdrData[0] == 0x0d):
-			break
-		fdHdr = struct.unpack("<11sciBB14x",fdHdrData)
-		#print(fdHdr)
-		(sFieldName, cFieldType,iFieldDataAddr,bFieldLength,bNumOfDeciPlaces) = fdHdr
-		sFieldName = sFieldName.decode().strip()
-		sFieldName = sFieldName.split('\x00')[0]
-		self.dbRecFields[sFieldName] = (iFOffset, cFieldType, bFieldLength)
-		iFOffset = iFOffset + bFieldLength
-		#print("INFO:FieldDescriptor\n\tsFieldName[{}]\n\tcFiledType[{}]\n\tbFieldLength[{}]".format(sFieldName,cFieldType,bFieldLength))
-	self.dbRecStartOffset = lenHdr
-	self.dbRecLen = lenRec
-	fsizeCalc = lenHdr+numRecs*lenRec
-	fsize = self.dbFile.seek(0,2)
-	if (fsizeCalc == fsize):
-		print("INFO: FileSize [{}] checks out wrt Header".format(fsize))
-	else:
-		print("ERROR: FileSize [{}] DOESNOT checks out wrt Header [{}]".format(fsize,fsizeCalc))
-	self.dbFile.seek(lenHdr)
-	print(self.dbRecFields)
+		hdrData = self.dbFile.read(32)
+		hdr = struct.unpack("<4Bi3h2B3i2Bh",hdrData)
+		#print(hdr)
+		(dbVer,uYY,uMM,uDD,numRecs,lenHdr,lenRec,hRsvd1,bDirty,bEncrypted,iFreeRecThread,iRsvd2,iRsvd3,bMdx,langDrvr,hRsvd4) = hdr
+		if (dbVer != 3):
+			print("ERROR: Dont understand the dBase dbf file format version...")
+			exit()
+		else:
+			print("INFO: dBase III w/o memo file, File without DBT")
+		print("INFO:FileHeader\n\tnumRecs[{}]\n\tlenHdr[{}]\n\tlenRec[{}]".format(numRecs,lenHdr,lenRec))
+		self.dbNumRecs = numRecs
+		self.dbRecStartOffset = lenHdr
+		self.dbRecLen = lenRec
+		iFOffset = 1 # This is to take care of the 1 byte record deleted or not flag at beginning of each record
+		while True:
+			fdHdrData = self.dbFile.read(32)
+			if (fdHdrData[0] == 0x0d):
+				break
+			fdHdr = struct.unpack("<11sciBB14x",fdHdrData)
+			#print(fdHdr)
+			(sFieldName, cFieldType,iFieldDataAddr,bFieldLength,bNumOfDeciPlaces) = fdHdr
+			sFieldName = sFieldName.decode().strip()
+			sFieldName = sFieldName.split('\x00')[0]
+			self.dbRecFields[sFieldName] = (iFOffset, cFieldType, bFieldLength)
+			iFOffset = iFOffset + bFieldLength
+			#print("INFO:FieldDescriptor\n\tsFieldName[{}]\n\tcFiledType[{}]\n\tbFieldLength[{}]".format(sFieldName,cFieldType,bFieldLength))
+		fsizeCalc = lenHdr+numRecs*lenRec
+		fsize = self.dbFile.seek(0,2)
+		if (fsizeCalc == fsize):
+			print("INFO: FileSize [{}] checks out wrt Header".format(fsize))
+		else:
+			print("ERROR: FileSize [{}] DOESNOT checks out wrt Header [{}]".format(fsize,fsizeCalc))
+		self.dbFile.seek(lenHdr)
+		print(self.dbRecFields)
 
-def dbf_read_record_field_bytes(f,iRecIndex,sFieldName):
-	self.dbFile.seek(self.dbRecStartOffset+iRecIndex*self.dbRecLen)
-	recData = self.dbFile.read(self.dbRecLen)
-	tOffset = self.dbRecFields[sFieldName][FIELD_OFFSET]
-	tLen = self.dbRecFields[sFieldName][FIELD_LEN]
-	fieldData = recData[tOffset:(tOffset+tLen)]
-	return fieldData
+	def dbf_read_record_field_bytes(self,iRecIndex,sFieldName):
+		self.dbFile.seek(self.dbRecStartOffset+iRecIndex*self.dbRecLen)
+		recData = self.dbFile.read(self.dbRecLen)
+		tOffset = self.dbRecFields[sFieldName][self.FIELD_OFFSET]
+		tLen = self.dbRecFields[sFieldName][self.FIELD_LEN]
+		fieldData = recData[tOffset:(tOffset+tLen)]
+		return fieldData
 
-def dbf_read_record_field_str(f,iRecIndex,sFieldName):
-	fieldData = dbf_read_record_field_bytes(f,iRecIndex,sFieldName)
+	def dbf_read_record_field_str(self,iRecIndex,sFieldName):
+		fieldData = self.dbf_read_record_field_bytes(iRecIndex,sFieldName)
+		try:
+			fieldData = fieldData.decode('ascii').strip()
+		except:
+			fieldData = "ERRORERRORERROR"
+		return fieldData
+
+	def dbf_check_field_exists(self,sFieldName):
+		if (self.dbRecFields.get(sFieldName) == None):
+			return False
+		return True	
+
+	def dbf_load(self, sFile):
+		self.dbFileName = sFile
+		self.dbFile=open(sFile,"rb")
+		self.dbf_read_fileheader()
+
+	def dbf_close(self):
+		self.dbFile.close()
+		self.__init__()
+
+
+if __name__ == "__main__":
+
+	db = DbfParser()
+	db.dbf_load(sys.argv[1])
+
 	try:
-		fieldData = fieldData.decode('ascii').strip()
-	except:
-		fieldData = "ERRORERRORERROR"
-	return fieldData
-
-
-def dbf_read(sFile):
-	self.dbFile=open(sFile,"rb")
-	dbf_read_fileheader(f)
-
-def dbf_close():
-	self.dbFile.close()
-
-def dbf_test():
-	f=dbf_read(sys.argv[1])
-	try:
-		print(self.dbRecFields["LONGITUDE"])
-		print(self.dbRecFields["LATITUDE"])
+		print("INFO: Field LONGITUDE:{}".format(db.dbRecFields["LONGITUDE"]))
+		print("INFO: Field LATITUDE:{}".format(db.dbRecFields["LATITUDE"]))
 		bCanPlot = True
 	except:
 		bCanPlot = False
@@ -104,21 +115,18 @@ def dbf_test():
 		gTr = turtle
 		gTr.penup()
 
-	for i in range(0,self.dbNumRecs):
+	for i in range(0,db.dbNumRecs):
 		if (bModeQueryField):
-			fdByte = dbf_read_record_field_bytes(f,i,sys.argv[2])
-			fdStr  = dbf_read_record_field_str(f,i,sys.argv[2])
+			fdByte = db.dbf_read_record_field_bytes(i,sys.argv[2])
+			fdStr  = db.dbf_read_record_field_str(i,sys.argv[2])
 			print(i,fdByte,fdStr)
 		if (bModePlot):
-			latY = float(dbf_read_record_field_str(f,i,"LATITUDE"))
-			lonX = float(dbf_read_record_field_str(f,i,"LONGITUDE"))
+			latY = float(db.dbf_read_record_field_str(i,"LATITUDE"))
+			lonX = float(db.dbf_read_record_field_str(i,"LONGITUDE"))
 			gTr.goto(lonX*3,latY*3)
 			gTr.dot()
 
-	dbf_close()
+	db.dbf_close()
 
-
-if __name__ = "__main__":
-	dbf_test()
 	input("Hope everything was fine...")
 
