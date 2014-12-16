05853a065c0299b4d8bc96f654a870f46b87c24f
hanishkvc <hanishkvc@gmail.com>
2014-12-16 11:50:40 +0530

    hkvc_plane.py
    * The shape of the plane which is maintained in a list of lists as a list of
      vertices IS NOW initialized directly into a polygon instance variable
      instead of being programmatically added to the list in the draw_plane func
    
      Also updated the shape of the plane.
    
      The 1st point/vertix in this list is required to be 0,0. So that when it is
      passed to the transform func (rotateAndTranslateBy) it doesn't distort the
      polygon being drawn.
    
      Also it is good for this 0,0 to be the middle (or near to it) of the shape
      if possible, so that the plane appears to be centered wrt the path being
      drawn behind it in the draw_path func.
    
      Also updated the shape of the plane to be oriented towards the North i.e
      Up by default, because the heading of the Plane is specified with North
      i.e Up considered as 0 degrees.
    
    * moved_to and draw_plane updated to take a additional argument wrt heading
      of the plane, which is now available from the MovingMap DataSources.
    
      * Updated draw_plane to use the specified heading to orient the image/shape
        of the polygon it plots.
    
    * moved_to func which saves the path of the plane updated to
    
      * Filter the path as it is adding/updating the path so that
        very small movements don't get saved into the path list,
        but instead get overwritten with the new path location
    
        NOTE that the default movement of a plane in normal flight condition
        also gets filtered/compressed/optimized so that only 1 in N path updates
        from the MovingMap Data Source gets saved. If you DONT want the normal
        flight path to be compressed then replace the current 0.02 check with
        something like 0.005
    
      * However Significant changes in path as well as one in every N path
        updates does get saved into the path list, so that the path does
        retain some amount of detail wrt the actual path traversed.
    
      * What is The interval of the 1 in N path updates to save is determined
        by the PLANE_PATH_FORCESAVEMOD.
    
        NOTE: Set this to 1 if each and every path update requires to be saved.
    
      * Additional provision added in the form of movedToCnt!=2 check to see to it
        that the very 1st point added to the path doesn't get overwritten by the
        Path optimisation / compression Filter logic.
    
      The above logics is to ensure that we have a compact but still sufficiently
      granular plane path

--- hkvc_plane.py
+++ hkvc_plane.py
@@ -4,24 +4,32 @@
 #
 
 PLANE_POS_DUMMY = 0x55AA55AA
+PLANE_PATH_FORCESAVEMOD = 8
 
 class Plane():
 
 	def __init__(self, plotter, x=0, y=0):
 		self.x = 0
 		self.y = 0
+		self.heading = 0
 		self.path = []
 		self.polygonId = None
 		self.pltr = plotter
+		self.movedToCnt = 0
+		# First point of polygon must be 0,0
+		#self.polygon = [[0,0],[5,5],[0,10]]
+		#self.polygon = [[0,0],[-5,5],[-5,-5]]
+		#self.polygon = [[0,0],[-5,-5],[-5,5]]
+		#self.polygon = [[0,0],[-5,-5],[5,-5]]
+		self.polygon = [[0,0],[-3,-10],[3,-10]]
 
-	def draw_plane(self, x=PLANE_POS_DUMMY, y=PLANE_POS_DUMMY):
+	def draw_plane(self, x=PLANE_POS_DUMMY, y=PLANE_POS_DUMMY, heading=PLANE_POS_DUMMY):
 		if ((x == PLANE_POS_DUMMY) or (y == PLANE_POS_DUMMY)):
 			x = self.x
 			y = self.y
-		points = list()
-		points.append([x,y])
-		points.append([x+5,y+5])
-		points.append([x,y+10])
+		if (heading == PLANE_POS_DUMMY):
+			heading = self.heading
+		points = self.pltr.rotateAndTranslateBy(self.polygon,heading,x,y)
 		if (self.polygonId == None):
 			self.pltr.color(250, 0, 0)
 			self.polygonId = self.pltr.polygon_noscale(points)
@@ -37,14 +45,27 @@ class Plane():
 		bShouldStroke=False
 		for i in range(1,len(self.path)):
 			[cX, cY] = self.path[i]
-			print("PlaneDrawPath:{}: {},{}".format(i, cX, cY))
+			#print("PlaneDrawPath:{}: {},{}".format(i, cX, cY))
 			self.pltr.line_to(cX, cY)
 			bShouldStroke=True
 		if (bShouldStroke):
 			self.pltr.stroke()
 
-	def moved_to(self, x, y):
+	def moved_to(self, x, y, heading=0):
+		self.movedToCnt += 1
+		cntMod = self.movedToCnt % PLANE_PATH_FORCESAVEMOD
 		self.x = x
 		self.y = y
-		self.path.append([x,y])
+		self.heading = heading
+		try:
+			[prevX, prevY] = self.path[-1]
+		except IndexError:
+			prevX = PLANE_POS_DUMMY
+			prevY = PLANE_POS_DUMMY
+		if (((abs(prevX-x) < 0.02) and (abs(prevY-y) < 0.02)) and (cntMod != 0) and (self.movedToCnt != 2)):
+			print("PLANE: Skipping Prev path loc")
+			self.path[-1] = [x,y]
+		else:
+			print("PLANE: Saving Prev path loc")
+			self.path.append([x,y])
 
